87e5c17173859163bb3650057982338eaa97a389
Cody Fulfer <cfulfer@yahoo.com>
2017-03-08 13:08:24 -0700

    Initial Commit of Repo/ Added FollowPath script and MovementPathScript

---
+++ Assets/Scripts/FollowPath.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour
+{
+    public MovementPath MyPath;
+    public float speed = 1;
+    public float maxDistanceToGoal = .1f;
+
+    private IEnumerator<Transform> pointInPath;
+
+	// Use this for initialization
+	void Start ()
+    {
+        InitiatePath();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MovePlayer();	
+	}
+
+    void InitiatePath()
+    {
+        if (MyPath == null)
+        {
+            Debug.LogError("Movement Path can not be null", gameObject);
+            return;
+        }
+
+        pointInPath = MyPath.GetNextPathPoint();
+        pointInPath.MoveNext();
+
+        if (pointInPath.Current == null)
+        {
+            Debug.LogError("No path", gameObject);
+            return;
+        }
+
+        transform.position = pointInPath.Current.position;
+    }
+
+    void MovePlayer()
+    {
+        if (pointInPath == null || pointInPath.Current == null)
+        {
+            return;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, pointInPath.Current.position, Time.deltaTime * speed);
+
+        var distanceSquared = (transform.position - pointInPath.Current.position).sqrMagnitude;
+        if (distanceSquared < maxDistanceToGoal * maxDistanceToGoal)
+        {
+            pointInPath.MoveNext();
+        } 
+    }
+}
---
+++ Assets/Scripts/MovementPath.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementPath : MonoBehaviour
+{
+
+    public int movementDirection = 1;
+    public int movingTo = 0;
+    public Transform[] PathSequence;
+
+    public void OnDrawGizmos()
+    {
+        if (PathSequence == null || PathSequence.Length < 2)
+        {
+            return;
+        }
+        for (var i = 1; i < PathSequence.Length; i++)
+        {
+            Gizmos.DrawLine(PathSequence[i - 1].position, PathSequence[i].position);
+        }
+    }
+    public IEnumerator<Transform> GetNextPathPoint()
+    {
+        if (PathSequence == null || PathSequence.Length < 1)
+        {
+            yield break;
+        }
+        while (true)
+        {
+            yield return PathSequence[movingTo];
+
+            if (PathSequence.Length == 1)
+            {
+                continue;
+            }
+            if (movingTo <= 0)
+            {
+                movementDirection = 1;
+            }
+            else if (movingTo >= PathSequence.Length - 1)
+            {
+                movementDirection = -1;
+            }
+
+            movingTo = movingTo + movementDirection;
+        }
+
+    }
+}
